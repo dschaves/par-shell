15bf9d10dce734e0045be87b9f9acb4f801c0caa
Sérgio <sergio.d.carvalho@tecnico.ulisboa.pt>
2015-09-29 10:54:49 +0100

    Update argumentgetter.c

--- argumentgetter.c
+++ argumentgetter.c
@@ -1,20 +1,27 @@
 char** divide_linha(char input[], int argc_max)
 {
-        char** tokens = malloc(sizeof(char)*(argc_max+1)); /* +1 para o comando */
-        char* token;
-        const char delimiters[] = " \t\n";
-        int i;
+        char** tokens = malloc(sizeof(char)*(argc_max+1));	/* +1 para o comando */
+        char* token;						/* cada token encontrado */
+        const char delimiters[] = " \t\n";			/* caracteres que acabam strtok */
+        int i;							/* indice do array */
 
-        token = strtok(input, delimiters); /* este token e o comando */
+        token = strtok(input, delimiters); 			/* este token e o comando */
 
-        for (i = 1 ; i <= argc_max && token != NULL ; i++)
+        for (i = 0 ; i <= argc_max && token != NULL ; i++)
+	/* Preencher o vector tokens com todos os tokens encontrados, ate ultrapassar o tamanho do vector ou chegar a um NULL.
+	* etc
+	*/	
         {
                 tokens[i] = token;
                 token = strtok(NULL, delimiters);
         }
 
-        if (i > argc_max)
-                return puts("Too many arguments given."), NULL;
+        if (tokens[i] != NULL)
+        {
+		free(tokens);		
+		puts("Too many arguments given.")
+		return NULL;
+	}
 
         return tokens;
 }
