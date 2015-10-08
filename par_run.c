63747c4064a1744a68a31339cd23155c26e0e6d5
Sérgio <sergio.d.carvalho@tecnico.ulisboa.pt>
2015-10-08 22:40:46 +0000

    Update par_run.c

--- par_run.c
+++ par_run.c
@@ -1,26 +1,19 @@
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "par_run.h"
 
-pid_t par_run(char* argv_child[])
-{
 
-    pid_t pid = fork(); //parent returns, child becomes desired process
-	
-	if (pid == 0) // is child
-	{	
-        /* we first try executing the command as a directory. If that does not work,
-		 * execute as a command in PATH.
-		 */
-		printf("%s",argv_child[0]);
-		execv(argv_child[0], argv_child); //arg 1: command; arg2: name and arguments
-		execvp(argv_child[0], argv_child);
-
-        // next instruction will not be reached if exec is successful
-        perror("par-shell: exec failed");
-        exit(1);
-    }
-    
-    else return pid; //is parent
+int par_run(char **argVector){
+	int pid = fork();
+	if (pid == -1){
+		perror("par-shell: unable to fork");
+		exit(EXIT_FAILURE);			
+	}
+	if (pid == 0){
+		execv(argVector[0], argVector);
+		perror("par-shell: exec failed");
+    	exit(-1);
+	}
+	return pid;
 }
