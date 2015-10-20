848b42a641616d776c46e328c3dc522824b15841
Sérgio <sergio.d.carvalho@tecnico.ulisboa.pt>
2015-10-20 12:08:43 +0100

    Mutexes added.

--- main.h
+++ main.h
@@ -1,14 +1,19 @@
 #ifndef PARSHELL_MAIN_H
 #define PARSHELL_MAIN_H
+
+#include <pthread.h>
+
 #define MAX_CHILDREN 8192
 
 struct main_status {
 
 	int exit_called;
 	int children_count;
+	pthread_mutex_t* mutex;
 	int children_pid_arr[MAX_CHILDREN];
 	int children_status_arr[MAX_CHILDREN];
 	time_t children_time_arr[MAX_CHILDREN];
+
  
 };
 #endif
