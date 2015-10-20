54a436639574fd2cb6313adfd49aaa5ffee9f403
Sérgio <sergio.d.carvalho@tecnico.ulisboa.pt>
2015-10-20 12:08:21 +0100

    Mutexes added.

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 #include "task_monitor.h"
 #include "main.h"
 
+//int a;
+
 #define INPUT_SIZE 7
 
 int main(int argc, char* argv[]) 
@@ -18,10 +20,13 @@ int main(int argc, char* argv[])
 
 	pthread_t monitor;
 
-	struct main_status main_status = {0,0};
+	pthread_mutex_t mutex;
+
+	pthread_mutex_init(&mutex, NULL);
+
+	struct main_status main_status = {0, 0, &mutex};
 
 	pthread_create(&monitor, NULL, task_monitor, &main_status);
-
 	puts("<< PAR-SHELL READY >>"); 
 
 	for(;;) // breaks upon "exit" input
@@ -42,20 +47,28 @@ int main(int argc, char* argv[])
 
 		else if (!strcmp(argv_child[0], "exit")) 
 		{
+			pthread_mutex_lock(&mutex);
 
 			main_status.exit_called += 1;
+
+			pthread_mutex_unlock(&mutex);
 
 			pthread_join(monitor, NULL);
 
 			int i;
-			for (i = 0; i < main_status.children_count; i++) // prints all pids and return values of children
+			for (i = 0; i < main_status.children_count; i++) // prints all pids, return values, and execution times of all children
 				printf("Process %d finished with status %d. Execution time: %d\n", 
-				main_status.children_pid_arr[i], main_status.children_status_arr[i], main_status.children_time_arr[i]);
+				main_status.children_pid_arr[i], main_status.children_status_arr[i], (int) main_status.children_time_arr[i]);
 
 			break;
 		}
 
-		else if (par_run(argv_child) != -1) main_status.children_count += 1; // new child created here
+		else if (par_run(argv_child) != -1) 
+		{
+			pthread_mutex_lock(&mutex);
+			main_status.children_count += 1; // new child created here
+			pthread_mutex_unlock(&mutex);
+		}
 	}
 
 	return 0;	
